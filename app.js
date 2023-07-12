23b956bb0af1ae3228e8622376b186d13060e5c8
chimlang <130890087+chimlang@users.noreply.github.com>
2023-07-12 13:34:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -165,8 +165,9 @@ io.on('connection', (socket) => {
         // ??
         // socket.handshake.headers["x-real-ip"] ??
         // socket.handshake.address;
-    // console.log(ipAddress)
-    // console.log(ipAlready)
+    console.log(socket.id)
+    console.log(ipAddress)
+    
 
     if (ipAlready[ipAddress]) {
         // if (isOneIpOneSocket) {
@@ -188,6 +189,8 @@ io.on('connection', (socket) => {
         ipAlready[ipAddress] = new Set()
         ipAlready[ipAddress].add(socket.id)
     }
+
+    console.log(ipAlready)
 
     //let nameNumber
     // Reconnecting
